37c1d72cae8bbf90a1c4d15768299e2514d98f2f
fahmi0604 <fahmifirmansyah917@gmail.com>
2022-10-02 21:25:29 +0700

    update login

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import AuthService from '../services/auth.service';
+import { navbarContext } from '../App';
 
 export default function NavbarComp() {
+
+    const [currentUser, setCurrentUser] = useState(undefined);
+    const [listNavbar, setListNavbar] = useState([]);
+    const navbarC = useContext(navbarContext);
+
+    useEffect(() => {
+        const user = AuthService.getCurrentUser();
+
+        if (user) {
+            console.log(user);
+            setCurrentUser(user);
+        } else {
+            console.log('tidak ada');
+        }
+
+        console.log(navbarC.navbar);
+    }, []);
+
     return (
-    <header className="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
-            <div className="container" style={{padding: '0px 32px'}}>
+        <header className="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
+            <div className="container" style={{ padding: '0px 32px' }}>
                 <div className="navbar-header">
                     <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span className="sr-only">Toggle navigation</span>
                         <span className="fa fa-bars"></span>
                     </button>
                     <a className="navbar-brand" href="https://simanta.umm.ac.id/~pkn_prod/">
-                        <h2 style={{marginTop:'10px'}}><i className="fa fa-home"></i> SIM PKN</h2>
+                        <h2 style={{ marginTop: '10px' }}><i className="fa fa-home"></i> SIM PKN</h2>
                     </a>
                 </div>
                 <div className="collapse navbar-collapse">
                     <ul className="nav navbar-nav navbar-right">
                         <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php">Home</a></li>
                         <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/pengumuman">Pengumuman</a></li>
-                        <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/panduan">Panduan</a></li>           
-                        
-                <li>
-                <a href="https://simanta.umm.ac.id/~pkn_prod/index.php/login" title="Login">Login</a>
-            </li>
-            <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/registrasi">Sign Up</a></li>
-        
+                        <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/panduan">Panduan</a></li>
+
+                        {navbarC.navbar ? '' : (
+                            <>
+                                <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/login" title="Login">Login</a></li>
+                                <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/registrasi">Sign Up</a></li>
+                            </>
+                        )}
+
                     </ul>
                 </div>
             </div>
-    
-    </header>
+
+        </header>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useContext, useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 import CarouseJumbotron from '../components/CarouselJumbotron'
+import AuthService from '../services/auth.service';
+
 export default function Home() {
+
+  // const [currentUser, setCurrentUser] = useState(undefined);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      console.log(user);
+    } else {
+      navigate('/');
+    }
+  }, []);
+
   return (
     <>
         <CarouseJumbotron />
