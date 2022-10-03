3cca6b84d307727ff56e0568860b3a62acf1c88c
ayuubb <ayubbkj2@gmail.com>
2022-10-03 22:55:36 +0700

    add portofolio

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/NavbarComp';
 import Footer from './components/FooterComp';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>  
             <Route path='/' element={<Login />} />
             <Route path='/home' element={<Home />} />
+            <Route path='/portfolio' element={<Portfolio />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         <Footer />
