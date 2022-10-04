42324dd2f6561cd2bff9c46d5b2a974de1cfbfb8
ayuubb <ayubbkj2@gmail.com>
2022-10-05 00:49:38 +0700

    add laporan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/FooterComp';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
+import Laporan from './pages/Laporan';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path='/' element={<Login />} />
             <Route path='/home' element={<Home />} />
             <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/laporan' element={<Laporan />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         <Footer />
