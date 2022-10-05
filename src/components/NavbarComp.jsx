6bad89ba208f96dc056a72fbc1c8a4596d799e78
ayuubb <ayubbkj2@gmail.com>
2022-10-05 17:01:17 +0700

    update router

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,33 +1,60 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export default function NavbarComp() {
     return (
-    <header className="navbar navbar-inverse navbar-fixed-top wet-asphalt" role="banner">
-            <div className="container" style={{padding: '0px 32px'}}>
+        <header
+            className="navbar navbar-inverse navbar-fixed-top wet-asphalt"
+            role="banner"
+        >
+            <div className="container" style={{ padding: '0px 32px' }}>
                 <div className="navbar-header">
-                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <button
+                        type="button"
+                        className="navbar-toggle"
+                        data-toggle="collapse"
+                        data-target=".navbar-collapse"
+                    >
                         <span className="sr-only">Toggle navigation</span>
                         <span className="fa fa-bars"></span>
                     </button>
-                    <a className="navbar-brand" href="https://simanta.umm.ac.id/~pkn_prod/">
-                        <h2 style={{marginTop:'10px'}}><i className="fa fa-home"></i> SIM PKN</h2>
+                    <a
+                        className="navbar-brand"
+                        href="https://simanta.umm.ac.id/~pkn_prod/"
+                    >
+                        <h2 style={{ marginTop: '10px' }}>
+                            <i className="fa fa-home"></i> SIM PKN
+                        </h2>
                     </a>
                 </div>
                 <div className="collapse navbar-collapse">
                     <ul className="nav navbar-nav navbar-right">
-                        <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php">Home</a></li>
-                        <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/pengumuman">Pengumuman</a></li>
-                        <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/panduan">Panduan</a></li>           
-                        
-                <li>
-                <a href="https://simanta.umm.ac.id/~pkn_prod/index.php/login" title="Login">Login</a>
-            </li>
-            <li><a href="https://simanta.umm.ac.id/~pkn_prod/index.php/registrasi">Sign Up</a></li>
-        
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        {/* <li>
+                            <a href='/pengumuman'>
+                                Pengumuman
+                            </a>
+                        </li> */}
+                        <li>
+                            <Link to="/portfolio">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="/laporan">Laporan</Link>
+                        </li>
+
+                        <li>
+                            <Link
+                                to="/login"
+                                title="Login"
+                            >
+                                Login
+                            </Link>
+                        </li>
                     </ul>
                 </div>
             </div>
-    
-    </header>
+        </header>
     );
 }
