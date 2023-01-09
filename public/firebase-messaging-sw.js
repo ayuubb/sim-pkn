e84307e1f09d64a2f098f4098e69d6c6963918da
ayuubb <ayubbkj2@gmail.com>
2023-01-09 22:05:00 +0700

    add notification firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,17 +1,31 @@
-// importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js");
+importScripts(
+    "https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js"
+);
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//       .then(function(registration) {
-//         console.log('Registration successful, scope is:', registration.scope);
-//       }).catch(function(err) {
-//         console.log('Service worker registration failed, error:', err);
-//       });
-//     }
+const firebaseConfig = {
+    apiKey: "AIzaSyBhpLAc9nr_nZhHFjvF6cueN31IO4j7pzc",
+    authDomain: "sim-pkn.firebaseapp.com",
+    projectId: "sim-pkn",
+    storageBucket: "sim-pkn.appspot.com",
+    messagingSenderId: "501182557899",
+    appId: "1:501182557899:web:044ebc631883b5c0b0f54e",
+    measurementId: "G-3DEMQSVWE7"
+};
 
-// firebase.initializeApp({
-//     messagingSenderId: "501182557899",
-//   })
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-// const initMessaging = firebase.messaging()
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        "[firebase-cloud-messaging-push-scope.js] Received background message ",
+        payload
+    );
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: payload.notification.image,
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
