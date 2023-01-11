7e4c871633863415e69bdc880329154b1192b7a2
ayuubb <ayubbkj2@gmail.com>
2023-01-11 16:15:41 +0700

    update url

--- src/services/API_URL.js
+++ src/services/API_URL.js
@@ -1,5 +1,5 @@
 // export default "https://api.belajariqra1.com/api";
 // export default 'https://192.168.1.32:3001/api';
-// export default "http://172.20.10.5:3001/api";
+export default 'http://172.20.10.5:3001/api';
 // export default "https://api.sistem-pembelajaran-audit.my.id/api"
-export default "http://localhost:3001/api"
+// export default "http://localhost:3001/api"
